0ff77b672805c5b9d91219c6b44fca2cf9a1b6e4
Kenny Chung <kcmac@Kennys-MacBook-Pro.local>
2018-08-20 19:35:08 -1000

    put and delete

--- server/routes/contacts.js
+++ server/routes/contacts.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const Contact = require('../db/models/Contact');
 const User = require('../db/models/User');
 
-router.get('/:username', (req, res) => {
+router.get('/users/:username', (req, res) => {
   const username = req.params.username;
   return User
     .query({ where: { username } })
@@ -22,12 +22,12 @@ router.get('/:username', (req, res) => {
 router.get('/search/:term', (req, res) => {
   const contact = req.params.term;
   return Contact
-  .query({ where: { name: contact }})
-  .fetchAll()
-  .then(contact => {
-    return res.json(contact);
-  })
-  .catch(err => console.log(err));
+    .query({ where: { name: contact } })
+    .fetchAll()
+    .then(contact => {
+      return res.json(contact);
+    })
+    .catch(err => console.log(err));
 });
 
 router.post('/', (req, res) => {
@@ -43,12 +43,50 @@ router.post('/', (req, res) => {
     github,
     created_by
   } = req.body;
-  return new Contact ({ name, address, mobile, work, home, email, twitter, instagram, github, created_by })
-  .save()
-  .then(newContact => {
-    return res.json(newContact);
-  })
-  .catch(err => console.log(err));
+  return new Contact({ name, address, mobile, work, home, email, twitter, instagram, github, created_by })
+    .save()
+    .then(newContact => {
+      return res.json(newContact);
+    })
+    .catch(err => console.log(err));
+});
+
+router.get('/:id', (req, res) => {
+  let id = req.params.id;
+  return Contact
+    .query({ where: { id } })
+    .fetchAll()
+    .then(contact => {
+      res.json(contact);
+    })
+    .catch(err => console.log(err));
+});
+
+router.put('/:id', (req, res) => {
+  let id = req.params.id;
+  let {
+    name,
+    address,
+    mobile,
+    work,
+    home,
+    email,
+    twitter,
+    instagram,
+    github,
+    created_by
+  } = req.body;
+  return Contact
+    .query({ where: { id } })
+    .fetchAll()
+    .then(contact => {
+      return new Contact({ id })
+        .save({ name, address, mobile, work, home, email, twitter, instagram, github, created_by })
+        .then(contact => {
+          return res.json(contact);
+        })
+    })
+    .catch(err => console.log(err));
 });
 
 module.exports = router;

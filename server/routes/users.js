a73af39f4e423c55a120c054de604d48c996e8db
Kenny Chung <kcmac@Kennys-MacBook-Pro.local>
2018-08-20 17:06:32 -1000

    get specific user

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,7 +1,14 @@
 const router = require('express').Router();
+const User = require('../db/models/User');
 
-router.get('/', (req, res) => {
-  res.send('users routes');
-})
+router.get('/:id', (req, res) => {
+  const id = req.params.id;
+  return User
+    .query({ where: { id } })
+    .fetchAll()
+    .then(user => {
+      return res.json(user);
+    })
+});
 
 module.exports = router;

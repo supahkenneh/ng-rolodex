eb1ccb93448da3c5038b45bd2482f013748cbb9a
Kenny Chung <kcmac@Kennys-MacBook-Pro.local>
2018-08-20 18:46:50 -1000

    get contacts created by users

--- server/routes/contacts.js
+++ server/routes/contacts.js
@@ -1,8 +1,21 @@
 const router = require('express').Router();
+const Contact = require('../db/models/Contact');
+const User = require('../db/models/User');
 
-router.route('/')
-  .get((req, res) => {
-    res.send('contacts routes');
-  })
+router.get('/:username', (req, res) => {
+  const username = req.params.username
+  return User
+    .query({ where: { username } })
+    .fetchAll()
+    .then(user => {
+      let id = user.models[0].id;
+      return Contact
+        .where({ created_by: id })
+        .fetchAll()
+        .then(contacts => {
+          return res.json(contacts);
+        });
+    });
+});
 
 module.exports = router;

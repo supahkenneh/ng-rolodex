83db8bca956fa90b1e5512561b8227af207dc79a
Kenny Chung <kcmac@Kennys-MacBook-Pro.local>
2018-08-21 17:05:46 -1000

    dinner

--- src/app/components/newcontact/newcontact.component.ts
+++ src/app/components/newcontact/newcontact.component.ts
@@ -1,10 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { BackendService } from '../../services/backend.service';
 
 @Component({
   templateUrl: './newcontact.component.html',
   styleUrls: ['./newcontact.component.scss']
 })
-export class NewContactComponent implements OnInit {
-  constructor() { }
-  ngOnInit() { }
+export class NewContactComponent {
+  formData: {
+    name: string,
+    address: string,
+    work: string,
+    home: string,
+    email: string,
+    twitter: string,
+    instagram: string,
+    github: string
+  } = {
+    name: '',
+    address: '',
+    work: '',
+    home: '',
+    email: '',
+    twitter: '',
+    instagram: '',
+    github: ''
+  };
+
+  nameValid: boolean = false;
+  numberValid: boolean = false;
+  emailValid: boolean  =false;
+  nameErrors: string[] = [];
+  emailErrors: string[] = [];
+  numberErrors: string[] = [];
+
+  constructor(private backend: BackendService) { }
+
+  validateName() {
+    this.nameErrors.length = 0;
+    if(!this.formData.name) {
+      this.nameErrors.push('Name is required');
+    }
+    else if (this.formData.name.length < 2) {
+      this.nameErrors.push('Minimum 2 characters required')
+    }
+    else { this.nameValid = true }
+  }
+
 }

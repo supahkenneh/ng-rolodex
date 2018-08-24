5193ef8230b958e864e58239fc6f6bace4bcd452
Kenny Chung <kcmac@kennys-mbp.gateway.ht.net>
2018-08-24 12:20:39 -1000

    handle home page if user is logged in/out

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { SessionService } from '../../services/session.service';
+import { Router } from '@angular/router';
 
 @Component({
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
+
 export class HomeComponent implements OnInit {
-  constructor() { }
-  ngOnInit() { }
+  isLoggedIn: boolean = false;
+
+  constructor(
+    private session: SessionService,
+    private router: Router
+  ) {
+    this.isLoggedIn = this.session.isLoggedIn();
+  }
+
+  ngOnInit() { 
+    if (this.isLoggedIn) {
+      this.router.navigate(['/profile'])
+    }
+   }
 
 
 }
